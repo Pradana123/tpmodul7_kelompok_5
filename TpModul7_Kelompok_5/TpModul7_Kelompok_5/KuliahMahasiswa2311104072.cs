be4f0f752cbfae025d7237db260a8a00ae1e02c4
jauharfz <jauharf14@gmail.com>
2025-04-14 17:31:51 +0700

    adding class KuliahMhs2311104072

---
+++ TpModul7_Kelompok_5/TpModul7_Kelompok_5/KuliahMahasiswa2311104072.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+public class KuliahMahasiswa2311104072
+{
+    public JSONFormat2 ReadJSON(string filename)
+    {
+        return JsonSerializer.Deserialize<JSONFormat2>(File.ReadAllText(filename));
+    }
+}
+
+public class JSONFormat2
+{
+    public List<CodeName> courses { get; set; }
+}
+
+public class CodeName
+{
+    public string code { get; set; }
+    public string name { get; set; }
+}
--- TpModul7_Kelompok_5/TpModul7_Kelompok_5/Program.cs
+++ TpModul7_Kelompok_5/TpModul7_Kelompok_5/Program.cs
@@ -2,11 +2,13 @@
 {
     static void Main(string[] args)
     {
-        DataMahasiswa2311104072 dataMahasiswa2311104072 = new DataMahasiswa2311104072();
-        var data = dataMahasiswa2311104072.ReadJSON("tp7_1_2311104072.json");
-
-        Console.WriteLine($"Nama {data.nama.depan} {data.nama.belakang} " +
-                          $"dengan nim {data.nim} " +
-                          $"dari fakultas {data.fakultas}");
+        KuliahMahasiswa2311104072 kuliahMahasiswa2311104072 = new KuliahMahasiswa2311104072();
+        var data = kuliahMahasiswa2311104072.ReadJSON("tp7_2_2311104072.json");
+        int index = 1;
+        foreach (var course in data.courses)
+        {
+            Console.WriteLine($"MK{index} {course.code} {course.name}");
+            index++;
+        }
     }
 }
