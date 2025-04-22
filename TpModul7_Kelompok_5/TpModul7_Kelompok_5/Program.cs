849edb3b9de298b985a21a38864e1a330835dff8
hazazaidan <fannzidna3@gmail.com>
2025-04-22 13:30:34 +0700

    merge

---
+++ TpModul7_Kelompok_5/TpModul7_Kelompok_5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TPModul7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa2311104056.ReadJSON();
+            KuliahMahasiswa2311104056.ReadJSON();
+        }
+    }
+}
